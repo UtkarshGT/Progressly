e6f34db2d76f51dc1e021fa86e99652eebd3c593
Utkarsh Tale <utkarshtale@gmail.com>
2022-04-18 01:55:49 +0530

    typo

--- Progressly/settings.py
+++ Progressly/settings.py
@@ -166,9 +166,6 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
